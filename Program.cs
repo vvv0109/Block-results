543f10fa8f6a544e340260d4e31144580f8a92e2
Vadim <qwe@ya.ru>
2022-10-10 15:39:13 +0300

    доработка

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
 ﻿Console.Clear(); //Очищаем терминал
 //int numA = Convert.ToInt32(Console.ReadLine());//Если хочешь включи, то пользователь сам будет задовать длинну масси
 string[] matrix = new string[5];
-int X = 3;
-int Y = 0;
+int X = 3, Y = 0;
 for (int i = 0; i < 5; i++)
 {
  Console.WriteLine($"Введите данные: ");
- string element = Convert.ToString(Console.ReadLine());
- if (element.Length <= X)
+ string numA = Convert.ToString(Console.ReadLine());
+ if (numA.Length <= X)
     {
-        matrix[Y] = element;
+        matrix[Y] = numA;
         Y++;
     }
 }
+Console.Write ("Итог: ");
 for (int i = 0; i < 5; i++)
     {
         Console.Write($"{matrix[i]} ");// выводим элемент массива
     }
-//Console.WriteLine();
-//PrintArray(matrix);
+
 
